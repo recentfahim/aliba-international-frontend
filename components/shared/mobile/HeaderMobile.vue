465c2b2c274f9a74bb0f17a0b8e27d8efd01291b
Fahim Ahmed <recentfahim13@gmail.com>
2020-11-18 01:40:41 +0600

    mobile header

--- components/shared/mobile/HeaderMobile.vue
+++ components/shared/mobile/HeaderMobile.vue
@@ -2,27 +2,13 @@
     <header class="header header--mobile">
         <div class="header__top">
             <div class="header__left">
-                <p>Welcome to Martfury Online Shopping Store !</p>
-            </div>
-            <div class="header__right">
-                <ul class="navigation__extra">
-                    <li>
-                        <nuxt-link to="/vendor/become-a-vendor">
-                            Sell on Martfury
-                        </nuxt-link>
-                    </li>
-                    <li>
-                        <nuxt-link to="/account/order-tracking">
-                            Tract your order
-                        </nuxt-link>
-                    </li>
-                </ul>
+                <p>Welcome to Aliba International Online Shopping Store !</p>
             </div>
         </div>
         <div class="navigation--mobile">
             <div class="navigation__left">
                 <nuxt-link to="/" class="ps-logo">
-                    <img src="~/static/img/logo_light.png" alt="martfury" />
+                    <img src="~/static/img/aliba_logo.jpeg" width="25%" alt="Aliba International" />
                 </nuxt-link>
             </div>
             <mobile-header-actions />
