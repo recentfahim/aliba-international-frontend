e994d7198d002665811aaa3e4c094648aad8281f
Fahim Ahmed <recentfahim13@gmail.com>
2020-11-19 02:29:10 +0600

    category style added in home page

--- components/partials/homepage/default/HomeBanner.vue
+++ components/partials/homepage/default/HomeBanner.vue
@@ -2,11 +2,20 @@
     <div class="ps-home-banner ps-home-banner--1">
         <div class="ps-container">
             <div class="ps-section__right pl-5">
-                <nuxt-link to="/shop" class="ps-collection" v-for="category in categories" :key="category.ID">
-                    <div>
-                        {{ category.Name }}
-                    </div>
-                </nuxt-link>
+                <ul role="menu" class="next-menu next-ver home-category">
+                    <li role="menuitem" aria-haspopup="true" aria-expanded="false" tabindex="0" class="next-menu-item" v-for="category in categories">
+                        <div class="next-menu-item-inner">
+                            <span class="next-menu-item-text">
+                                <div class="c-menu">
+                                    <p>
+                                        <a href="/shop" class="f-14 c-name">{{ category.Name }}</a>
+                                    </p>
+                                </div>
+                            </span>
+                            <i class="next-icon next-icon-arrow-right next-medium next-menu-icon-arrow"></i>
+                        </div>
+                    </li>
+                </ul>
             </div>
             <div class="ps-section__left ml-4">
                 <div class="ps-carousel" v-swiper:mySwiper="swiperOption">
@@ -85,5 +94,94 @@ export default {
 <style lang="scss" scoped>
 .ps-section__left{
     max-height: 425px;
+}
+.next-menu, .next-menu *, .next-menu :after, .next-menu :before {
+    -webkit-box-sizing: border-box;
+    box-sizing: border-box;
+}
+.next-menu {
+    position: relative;
+    min-width: 100px;
+    margin: 0;
+    list-style: none;
+    border: 1px solid #dcdee3;
+    border-radius: 3px;
+    -webkit-box-shadow: none;
+    box-shadow: none;
+    line-height: 32px;
+    font-size: 12px;
+    -webkit-animation-duration: .3s;
+    animation-duration: .3s;
+    -webkit-animation-timing-function: ease;
+    animation-timing-function: ease;
+}
+
+.home-category {
+    width: 300px;
+    border: 0!important;
+    -webkit-box-shadow: none;
+    box-shadow: none;
+}
+
+.home-category .next-menu-item, .home-category .next-menu-item-inner {
+    height: 51px;
+}
+
+.home-category .next-menu-item {
+    padding: 0;
+}
+
+.next-menu-item {
+    position: relative;
+    padding: 0 20px;
+    -webkit-transition: background .1s linear;
+    -o-transition: background .1s linear;
+    transition: background .1s linear;
+    color: #333;
+    cursor: pointer;
+}
+
+.next-menu-item:hover{
+    a{
+        color: red;
+    }
+}
+
+.home-category .next-menu-item .next-menu-item-inner {
+    border-left: 2px solid #fff;
+}
+
+.home-category .next-menu-item, .home-category .next-menu-item-inner {
+    height: 35px;
+}
+
+.next-menu-item-inner {
+    height: 32px;
+    font-size: 12px;
+    overflow: hidden;
+    -o-text-overflow: ellipsis;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    word-wrap: normal;
+}
+
+.next-menu-item-text {
+    vertical-align: middle;
+}
+
+.home-category .c-menu {
+    padding: 5px 22px 0;
+}
+
+.home-category p {
+    margin: 0;
+    -o-text-overflow: ellipsis;
+    text-overflow: ellipsis;
+    overflow: hidden;
+}
+
+.home-category .c-name {
+    margin-right: 10px;
+    font-weight: 700;
 }
 </style>
