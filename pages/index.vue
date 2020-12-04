6916500dc5da5dc82776de921b89b5d95eada2f0
recnetfahim <recentfahim13@gmail.com>
2020-12-05 03:01:06 +0600

    remove unnessary items from home

--- pages/index.vue
+++ pages/index.vue
@@ -14,24 +14,6 @@
 </template>
 <script>
 import { mapState } from 'vuex';
-import DownloadApp from '~/components/partials/commons/DownloadApp';
-import FooterFullwidth from '~/components/shared/footers/FooterFullwidth';
-import Newsletters from '~/components/partials/commons/Newsletters';
-import SiteFeauturesFullwidth from '~/components/partials/commons/SiteFeauturesFullwidth';
-import HomeAdsColumns from '~/components/partials/homepage/default/HomeAdsColumns';
-import HomeAds from '~/components/partials/homepage/default/HomeAds';
-import NewArrivals from '~/components/partials/homepage/default/NewArrivals';
-import HomeDefaultTopCategories from '~/components/partials/homepage/default/HomeDefaultTopCategories';
-import GardenAndKitchen from '~/components/partials/homepage/default/GardenAndKitchen';
-import Clothings from '~/components/partials/homepage/default/Clothings';
-import ConumerElectronics from '~/components/partials/homepage/default/ConumerElectronics';
-import HomeBanner from '~/components/partials/homepage/default/HomeBanner';
-import HeaderDefault from '~/components/shared/headers/HeaderDefault';
-import NavigationList from '~/components/shared/mobile/NavigationList';
-import HeaderMobile from '~/components/shared/mobile/HeaderMobile';
-import MobileDrawer from '~/components/shared/mobile/MobileDrawer';
-import HomeDefaultDealOfDay from '~/components/partials/homepage/default/HomeDefaultDealOfDay';
-import DemoPanel from '~/components/shared/DemoPanel';
 import ProductList from '~/components/partials/homepage/default/ProductList';
 import ProductGridView from '~/components/product/product-grid-view';
 import FlashSale from '~/components/product/flash-sale';
@@ -39,49 +21,13 @@ import TopRanking from '~/components/product/TopRanking';
 
 export default {
     components: {
-        DemoPanel,
-        HomeDefaultDealOfDay,
-        MobileDrawer,
-        HeaderMobile,
-        NavigationList,
-        HeaderDefault,
-        HomeBanner,
-        GardenAndKitchen,
-        HomeAdsColumns,
-        SiteFeauturesFullwidth,
-        HomeAds,
-        FooterFullwidth,
-        DownloadApp,
-        Newsletters,
-        NewArrivals,
-        HomeDefaultTopCategories,
-        Clothings,
-        ConumerElectronics,
         ProductList,
         ProductGridView,
         FlashSale,
         TopRanking
     },
-
     transition: 'zoom',
     layout: 'layout-default',
-
-    computed: {
-        ...mapState({
-            collections: state => state.collection.collections
-        })
-    },
-
-    async created() {
-        const queries = [
-            'deal-of-the-day',
-            'consumer-electronics',
-            'clothings',
-            'garden-and-kitchen',
-            'new-arrivals-products'
-        ];
-        await this.$store.dispatch('collection/getCollectionsBySlugs', queries);
-    }
 };
 </script>
 <style scoped>
